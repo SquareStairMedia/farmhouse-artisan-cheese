5ec5fd0cf7bb44136e66abed563dabfe5f900413
Stephen <mcdermott.stephen.c@gmail.com>
2025-12-21 21:12:06 -0500

    added newsletter file, updated security headers

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const { Resend } = require('resend');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 require('dotenv').config();
 
 const app = express();
+app.use(helmet());
 const PORT = process.env.PORT || 3000;
 
 // Initialize Resend
